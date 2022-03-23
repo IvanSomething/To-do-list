63b32b66ae5aad99bb61bbc68bf097b02145c088
IvanShklovski <ivan.shklovski@digitalizm.com>
2022-03-23 19:29:30 +0300

    Create new app

---
+++ db/migrate/20220323162301_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, null: false
+      t.text :description, null: false
+      t.integer :status, default: 0
+
+      t.timestamps
+    end
+  end
+end
